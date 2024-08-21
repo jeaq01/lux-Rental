dac3334522dd59b2c52ce405515e1d4e666379f6
Joshua Nanninga <jmarknanninga@gmail.com>
2024-08-20 21:18:27 -0500

    added Nunito

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
 }
 
