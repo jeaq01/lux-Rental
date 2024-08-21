8fcc608e2babca825bf7e1287814172b6efbdfe7
Cassidy Pagels <rozeneclipse@gmail.com>
2024-08-20 19:43:18 -0500

    adding data to seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,7 +6,28 @@ const locationData = require('./locationData.json')
 const carData = require('./carData.json')
 
 const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  })
+
+  for (const location of locationData) {
+    await Location.create({
+      ...location,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    })
+  }
+
+  for (const car of carData) {
+    await Car.create({
+      ...car,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    })
+  }
+
+  process.exit(0)
 }
 
 seedDatabase()
